c0eca178f72017c455654c6e24d47ed3e0426dc1
Gonzalo Martone <gmartone@itba.edu.ar>
2022-08-08 01:29:43 -0300

    more keymaps

--- nvim/.config/nvim/lua/gone/keymaps.lua
+++ nvim/.config/nvim/lua/gone/keymaps.lua
@@ -21,6 +21,10 @@ vim.g.maplocalleader = " "
 --  term_mode           = "t"
 --  command_mode        = "c"
 
+-- C -> ctrl
+-- S -> shift
+-- A -> alt
+
 -- Normal mode
 --  Better window movement
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -28,16 +32,25 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+--  Move lines
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+
 --  Leader shortcuts
 keymap("n", "<leader>e", ":Lex 30<CR>", opts)
+keymap("n", "<leader>r", ":!gcc -pedantic -std=c99 -Wall hello.c & ./a.out<CR>", opts)
+
+-- Insert Mode
+--  Move lines
+keymap("i", "<A-j>", "<ESC>:m .+1<CR>==gi", opts)
+keymap("i", "<A-k>", "<ESC>:m .-2<CR>==gi", opts)
 
 -- Visual mode
 --  Indent lines with ">" and "<"
 keymap("v", ">", ">gv", opts)
 keymap("v", "<", "<gv", opts)
 
---  Move lines up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-
+--  Move lines -- redo :)
+--keymap("v", "<A-j>", "<ESC>:m .+1<CR>==gv", opts)
+--keymap("v", "<A-k>", "<ESC>:m .-2<CR>==gv", opts)
 
